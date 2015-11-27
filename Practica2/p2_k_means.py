06ea357aadf0ba66618ca41ffa32d97dd73016e4
Alberto Lorente Sanchez <alberto.lorente92@gmail.com>
2015-11-27 16:45:39 +0100

    Datos. P2. Kmeans centroides.

---
+++ Practica2/p2_k_means.py
@@ -0,0 +1,44 @@
+import csv
+from scipy.spatial import distance
+#from collections import Counter
+
+def read_file(filename):
+  infile = open(filename, 'r')
+  reader = csv.reader(infile)
+  lista = list(reader)
+  return [[int(y) for y in x] for x in lista[1:]]
+
+def kmeans(k, instancias, centroides_ini = None):
+  pass  
+
+
+def getCentroids(k, instancias):
+  listaCentroids = [instancias[0]]
+  for i in range(1,k):
+    listaDistancias = []
+    for ins in instancias:
+      listaDistancias.append(getDistancia(ins,listaCentroids))
+    max_pos = listaDistancias.index(max(listaDistancias))
+    listaCentroids.append(instancias[max_pos])
+  return listaCentroids
+
+      
+
+def getDistancia(i1, lista_ins):
+  dist = 0.0
+  for ins in lista_ins:
+    dist+=distance.euclidean(i1,ins)
+  return dist
+
+
+
+
+
+
+
+def prueba():
+  aux = read_file('customers.csv')
+  listaCentros = getCentroids(4,aux)
+  print listaCentros
+
+prueba()
