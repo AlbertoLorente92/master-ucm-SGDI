95f422e6534d0c9e63e0df5a0718aa27afaa0e82
AlbertoLorente92 <alberto.lorente92@gmail.com>
2015-11-27 18:15:07 +0100

    Datos. P2. KMeans funcionando.

--- Practica2/p2_k_means.py
+++ Practica2/p2_k_means.py
@@ -1,6 +1,8 @@
 import csv
 from scipy.spatial import distance
 #from collections import Counter
+import numpy as np
+
 
 def read_file(filename):
   infile = open(filename, 'r')
@@ -11,8 +13,30 @@ def read_file(filename):
 def kmeans(k, instancias, centroides_ini = None):
   if centroides_ini == None:
     centroides_ini = getCentroids(k,instancias)
-  
-  
+  oldListCluster = [[] for x in range(0,k)]
+  while True:
+    newListCluster = getCluster(centroides_ini,instancias)
+    if newListCluster == oldListCluster:
+      break
+    oldListCluster = newListCluster
+    centroides_ini = recalcular_centroides(newListCluster)
+  return (newListCluster , centroides_ini)
+
+def getCluster(centroids, instancias):
+  listCluster = [[] for x in range(0,len(centroids))]
+  for ins in instancias:
+    index = closestCentroid(ins,centroids)
+    listCluster[index].append(ins)
+  return listCluster
+
+def recalcular_centroides(newListCluster):
+  listaAux = []
+  for lista in newListCluster:
+    lista = np.array(lista)
+    aux2 = np.mean(lista, axis=0).tolist()
+    listaAux.append(aux2)
+  return listaAux
+
 def closestCentroid(ins, centroides_ini):
   listaDistancias = []
   for cent in centroides_ini:
@@ -29,23 +53,17 @@ def getCentroids(k, instancias):
     listaCentroids.append(instancias[max_pos])
   return listaCentroids
 
-      
-
 def getDistancia(i1, lista_ins):
   dist = 0.0
   for ins in lista_ins:
     dist+=distance.euclidean(i1,ins)
   return dist
 
-
-
-
-
-
-
 def prueba():
   aux = read_file('customers.csv')
-  listaCentros = getCentroids(4,aux)
-  print listaCentros
+  (clusters, centroids) = kmeans(4,aux)
+  for clus, cent in zip(clusters, centroids):
+    print "cent: ",cent 
+    print "clus: ",clus
 
 prueba()
