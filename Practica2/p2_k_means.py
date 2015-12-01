abf430a8836e4b5ac87df9909a471cfb38c4bb4b
Hristo Ivanov <hristo.idgh@gmail.com>
2015-12-01 20:17:54 +0100

    Datos. P2. K_Means Terminado.

--- Practica2/p2_k_means.py
+++ Practica2/p2_k_means.py
@@ -44,7 +44,7 @@ def recalcular_centroides(newListCluster,centroides):
 
 def getCentroids(k, instancias):
   listaCentroids = [instancias[0]]
-  for i in range(1,k):
+  for i in range(0,k):
     listaDistancias = []
     for ins in instancias:
       listaDistancias.append(getDist(ins,listaCentroids))
@@ -68,11 +68,7 @@ def getMinMaxDistFromList(listaCluster, theFunc = max):
   distMin = -1.0
   for x in range(0,len(listaCluster)):
     for y in range(x+1,len(listaCluster)):
-      aux = distance.euclidean(listaCluster[x],listaCluster[y]) ##Si solo hay 2 instancias esto da = nan
-      if len(listaCluster) == 2: # No se, creo que esto sobra, imprimiendo antes y después veo los mismos valores.
-        print(aux,"---->>",listaCluster[x], "---",listaCluster[y])   
-        aux = distanciaAMano(listaCluster[x],listaCluster[y])  ##pero si lo calculo todo manual funciona
-        print aux
+      aux = distance.euclidean(listaCluster[x],listaCluster[y])
       if aux > distMin:
         distMin = aux
   return distMin
@@ -81,7 +77,7 @@ def getAverageDistance(centroid, cluster):
   dist = 0.0
   for instance in cluster:
     dist = dist + math.pow(distance.euclidean(instance,centroid), 2)
-  return dist//len(cluster)
+  return dist/float(len(cluster))
 
 def getRadio(clus, cent):
   if len(clus) <= 1: return 0.0
@@ -116,50 +112,62 @@ def getDistancias(clusters, centroids):
 def getStats(lista):
   return [ np.mean(lista)-np.std(lista), np.mean(lista)+np.std(lista), min(lista), max(lista) ]
 
-def distanciaAMano(ins1,ins2):
-  a1 = math.pow(math.fabs(ins1[0]-ins2[0]),2)
-  a2 = math.pow(math.fabs(ins1[1]-ins2[1]),2)
-  a3 = math.pow(math.fabs(ins1[2]-ins2[2]),2)
-  a4 = math.pow(math.fabs(ins1[3]-ins2[3]),2)  
-  a5 = math.pow(math.fabs(ins1[4]-ins2[4]),2)  
-  a6 = math.pow(math.fabs(ins1[5]-ins2[5]),2)
-  suma = a1+a2+a3+a4+a5+a6
-  total = math.sqrt(suma)
-  return total
+def getMeanWeight(listaRadios, listaPesos, total):
+  auxSum = 0.0
+  for rad, weight in zip(listaRadios, listaPesos):
+    auxSum += rad * (weight/float(total))
+  return auxSum
   
 def prueba():
   aux = read_file('customers.csv')
-  radios = []
-  diametros = []
-  distancias = []
+  radios         = []
+  radiosNorm     = [None, None]
+  diametros      = []
+  diametrosNorm  = [None, None]
+  distancias     = []
+  distanciasNorm = [None, None]
   
-  for i in xrange(2,20):
+  for i in range(2,21):
     (clusters, centroids) = kmeans(i,aux)
     listRadios     = getRadios(clusters, centroids)
-    listDiametros  = getDiametros(clusters)
-    listDistancias = getDistancias(clusters, centroids)
     radios.append(     [i]+getStats(listRadios))
-    diametros.append(  [i]+getStats(listDiametros))
-    distancias.append( [i]+getStats(listDistancias))
+    radioNorm = getMeanWeight(listRadios, [len(x) for x in clusters], len(aux))
+    radiosNorm.append(radioNorm)
 
-  print radios
+    listDiametros  = getDiametros(clusters)
+    diametros.append(  [i]+getStats(listDiametros))
+    diametroNorm = getMeanWeight(listDiametros, [len(x) for x in clusters], len(aux))
+    diametrosNorm.append(diametroNorm)
+
+    listDistancias = getDistancias(clusters, centroids)    
+    distancias.append( [i]+getStats(listDistancias))
+    distanciaNorm = getMeanWeight(listDistancias, [len(x) for x in clusters], len(aux))
+    distanciasNorm.append(distanciaNorm)
+    if i == 5:
+      print '===>', listDistancias.index(max(listDistancias))
+      print max(listDistancias)
+      print clusters[listDistancias.index(max(listDistancias))]
+
+
   fig, ax = plt.subplots()
   fig.subplots_adjust(bottom=0.2)
   candlestick(ax, radios, width=0.2, alpha=0.5)
-  plt.show()
-
-  """ 
-  plt.plot(listRadios)
-  plt.xlabel('K-centros')
+  plt.plot(radiosNorm)
+  plt.axhline(0, color='red')
   plt.savefig('Radios.png')
-  
-  plt.plot(listDiametros)
-  plt.xlabel('K-centros')
+
+  fig, ax = plt.subplots()
+  fig.subplots_adjust(bottom=0.2)
+  candlestick(ax, diametros, width=0.2, alpha=0.5)
+  plt.plot(diametrosNorm)
+  plt.axhline(0, color='red')
   plt.savefig('Diametros.png')
-  
-  plt.plot(listDistancia)
-  plt.xlabel('K-centros')
+
+  fig, ax = plt.subplots()
+  fig.subplots_adjust(bottom=0.2)
+  candlestick(ax, distancias, width=0.2, alpha=0.5)
+  plt.plot(distanciasNorm)
+  plt.axhline(0, color='red')
   plt.savefig('Distancia.png')
-  """
 
 prueba()
