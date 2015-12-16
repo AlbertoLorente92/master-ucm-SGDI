cfb3e5dec84548290026ed049b31ef8d1d75ecbb
Hristo Ivanov <hristo.idgh@gmail.com>
2015-12-16 16:50:23 +0000

    Datos. P2. Comentarios.

--- Practica2/p2_id3.py
+++ Practica2/p2_id3.py
@@ -12,14 +12,13 @@ def read_file(filename):
   inst = [[y for y in x] for x in lista[1:]]
   aux = lista[0]
   attrib_dic = {}
-  # Importante. En la linea de Abajo he anadido un -1.
-  # Es necesario para no coger las clases, solo los atributos.
   for i in range(0,len(aux)-1):
     aux2 = list(set([x[i] for x in lista[1:]]))
     attrib_dic[aux[i]] = (i , aux2)	
   classes =list(set([x[-1] for x in lista[1:]]))
   return (inst,attrib_dic,classes)
 
+# Algoritmo id3
 def id3(inst, attrib_dic, classes, candidates):
   listClases = [x[-1] for x in inst]
   if len(set(listClases)) == 1:
@@ -35,11 +34,11 @@ def id3(inst, attrib_dic, classes, candidates):
     if len(subConj) == 0:
       hoja = {'nombre' : claseModa}
     else:
-      #hoja = id3(subConj, attrib_dic, classes, candidates.pop(attr))
       hoja = id3(subConj, attrib_dic, classes, {k:v for k,v in candidates.iteritems() if k != attr})
     arbol['hijos'][val] = hoja
   return arbol
 
+# Calcula la entropia de una lista.
 def entr(tList):
   entrSum = 0.0
   tLen = float(len(tList))
@@ -48,6 +47,7 @@ def entr(tList):
     entrSum += -(val/tLen * math.log(val/tLen,2))
   return entrSum
 
+# Devuelve la entropia de un atributo.
 def entrAttr(attr, tList):
   lenList = len(tList)
   sumEntr = 0.0
@@ -56,20 +56,24 @@ def entrAttr(attr, tList):
     sumEntr += entr([x[-1] for x in subConj]) * len(subConj) / lenList
   return sumEntr
 
+# Devuelve el nombre del attributo que mayor ganancia de entropia
+# aporta.
 def selecciona_atributo(attrDict, tList):
   attrListEntr = {}
   for attrName, attrInfo in attrDict.iteritems():
     attrListEntr[attrName] = entrAttr(attrInfo, tList)
   return min(attrListEntr, key=attrListEntr.get)
   
+# Devulve un subconjunto tal que, el atributo indicado por
+# attrIndex siempre toma el valor definido por attrVal.
 def getSubConjunto(attrIndex, attrVal, tList):
   return [x for x in tList if x[attrIndex] == attrVal]
  
 def prueba():
   inst,attrib_dic,classes = read_file('car.csv')
-  aaa = id3(inst, attrib_dic, classes, attrib_dic)
-  print json.dumps(aaa, indent=4)
-  write_dot_tree(aaa, 'aux.dot')
+  arbol = id3(inst, attrib_dic, classes, attrib_dic)
+  #print json.dumps(arbol, indent=4)
+  write_dot_tree(arbol, 'aux.dot')
 
 def write_dot_tree(id3_tree, filename):
   to_write = 'digraph graphname {\n'
@@ -79,6 +83,8 @@ def write_dot_tree(id3_tree, filename):
   fd.write(to_write)
   fd.close()
 
+# Devuelve un string que define el arbol formado por
+# el algoritmo id3.
 def getDot(tDict, count = [0]):
   out = ''
   nombreOld = tDict['nombre'] 
@@ -92,16 +98,3 @@ def getDot(tDict, count = [0]):
   return out
 
 prueba()
-
-# Importante: Lo que esta bien.
-#   -> Las funciones 'read_file' y 'entr' que hicimos juntos.
-#      En la funcion 'read_file' he hecho una pequena correcion.
-#   -> El array 'aux' con los valores de las diapos de ID3. Creo que vendra bien para probar el funcinamiento de esto.
-#   -> La funcion 'getSubConjunto' que hace lo que indica el nombre.
-#      Se invoca de la siguiente manera :  subConj = getSubConjunto(0, 1, aux)
-#        Esto devuelve las filas que tienen valor 1 en la columna 0, las 8 primeras de aux.
-#   -> La funcion 'entrAttr' que calcula la entropia para un atributo.
-#      Se invoca de la siguiente manera :  entrAttr( (0, [1,2,3]), aux) . Esto devuelve 1.28669102172.
-#   -> La funcion 'selecciona_atributo' devuelve el 'key' del atributo que aporta mayor ganancia.
-
-# Con esto yo creo que quedaria tan solo implementar la funcion recursiva. La que genera el arbol.
