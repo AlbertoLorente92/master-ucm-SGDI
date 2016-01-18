f4e05e995466b0e433cd19e28cb189c811bad8b2
AlbertoLorente92 <alberto.lorente92@gmail.com>
2016-01-18 16:37:53 +0100

    SGDI. P3. Aggregation pipeline

---
+++ Practica3/consultas.js
@@ -0,0 +1,66 @@
+/*******************************************************************************
+**************************** AGGREGATION FRAMEWORK *****************************
+*******************************************************************************/
+
+// Listado de pais-numero de usuarios ordenado de mayor a menor por numero de 
+// usuarios.
+function agg1(){
+  db.sgdi.aggregate([{$group:{_id:"$country","num_users":{$sum:1}}},{$sort:{"num_users":-1}}])
+  return "";
+}
+
+
+// Listado de pais-numero total de posts de los 3 paises con mayor numero total 
+// de posts, ordenado de mayor a menor por numero de posts.
+function agg2(){
+  db.sgdi.aggregate([{$group:{_id:"$country","num_posts":{$sum:"$num_posts"}}},{$sort:{"num_posts":-1}},{$limit:3}])
+	return "";
+}
+
+  
+// Listado de aficion-numero de usuarios ordenado de mayor a menor numero de 
+// usuarios.
+function agg3(){
+	return "";
+}  
+  
+  
+// Listado de aficion-numero de usuarios restringido a usuarios espanoles y
+// ordenado de mayor a menor numero de usuarios.
+function agg4(){
+  return "";
+}
+
+
+
+/*******************************************************************************
+********************************** MAPREDUCE ***********************************
+*******************************************************************************/
+  
+// Listado de aficion-numero de usuarios restringido a usuarios espanoles.
+function mr1(){
+	return "";
+}
+
+
+// Listado de numero de aficiones-numero de usuarios, es decir, cuAntos
+// usuarios tienen 0 aficiones, cuantos una aficion, cuantos dos aficiones, etc.
+function mr2(){
+	return "";
+}
+
+
+// Listado de pais-numero de usuarios que tienen mas posts que contestaciones.
+function mr3(){
+	return "";
+}
+
+
+// Listado de pais-media de posts por usuario.
+function mr4(){
+	return "";
+}
+
+
+
+
